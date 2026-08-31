0a633263b4c09a878988a394c8137391e2327f2c
Tom Himler <thimler9@gmail.com>
2026-08-31 18:28:29 -0400

    Plugin setup

--- src/main/java/com/bingo_auto_info_upload/BingoAutoInfoUploadPlugin.java
+++ src/main/java/com/bingo_auto_info_upload/BingoAutoInfoUploadPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.bingo_auto_info_upload;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Bingo Auto Info Upload"
 )
-public class ExamplePlugin extends Plugin
+public class BingoAutoInfoUploadPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BingoAutoInfoUploadConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BingoAutoInfoUploadConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BingoAutoInfoUploadConfig.class);
 	}
 }
--- src/test/java/com/bingo_auto_info_upload/BingoAutoInfoUploadPluginTest.java
+++ src/test/java/com/bingo_auto_info_upload/BingoAutoInfoUploadPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bingo_auto_info_upload;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BingoAutoInfoUploadPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BingoAutoInfoUploadPlugin.class);
 		RuneLite.main(args);
 	}
 }
